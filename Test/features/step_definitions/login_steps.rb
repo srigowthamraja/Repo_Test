22abdc94bca4a5347241c35bf2b3d8420b2591ee
Gowtham <gowthamraja247@gmail.com>
2014-04-06 22:29:50 +0530

    Demo first

--- Test/features/step_definitions/login_steps.rb
+++ Test/features/step_definitions/login_steps.rb
@@ -18,8 +18,38 @@ Then(/^I'm able to see "(.*?)"$/) do |page_title|
   find("div.text").should have_text(page_title)  
 end
 
+Given(/^click project settings$/) do
+ page.find(:xpath, '//div[contains(., "master")]')
+end
 
+When(/^enter a project name as (.*)$/) do |project_name|
+  fill_in "ctl00_ContentPlaceHolder1_txtProjectName",:with=>project_name
+end
 
- 
-  
+When(/^enter rewards points as (.*)$/) do |reward_points|
+  fill_in "ctl00_ContentPlaceHolder1_txtRewardPoints_text",:with=>reward_points
+end
 
+When(/^enter survey length as (.*)$/) do |survey_length|
+  fill_in "ctl00_ContentPlaceHolder1_txtSurveyLength_text",:with=>survey_length
+end
+
+When(/^enter live date as (.*)$/) do |live_date|
+  fill_in "ctl00_ContentPlaceHolder1_txtLiveDate_dateInput_text",:with=>live_date
+end
+
+And(/^enter close date as (.*)$/) do |close_date|
+  fill_in "ctl00_ContentPlaceHolder1_txtCloseDate_dateInput_text",:with=>close_date
+end
+
+And(/^enter project description as (.*)$/) do |project_description|
+  fill_in "ctl00_ContentPlaceHolder1_txtProjectDescription",:with=>project_description
+end
+
+And(/^click save button$/) do
+  click_on "ctl00_ContentPlaceHolder1_ibtnSurveySetting"
+end
+
+Then(/^I'm able to see project created successfully "(.*?)"$/) do |page_title|
+  find("div.text").should have_text(page_title)  
+end
--- Test/features/test.feature
+++ Test/features/test.feature
@@ -1,4 +1,4 @@
-Feature: First Test
+#Feature: First Test
 
 Scenario: login test
 Given launch app
@@ -7,3 +7,15 @@ And enter password as 123
 And click Login button
 Then I'm able to see "Project Dashboard"
 
+#Feature: Project settings Test
+
+Scenario: Create a survey
+Given click project settings
+When enter a project name as Dailytest
+When enter reward points as 1
+When enter survey length as 2
+When enter live date as 20-12-2013
+And enter close date as 30-12-2013
+And enter project description as Dailytest
+And Click Save button
+Then I'm able to see "Project created successfully"
